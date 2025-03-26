541e06d1112b094f7be85099680c19b6d2cd289a
miguelax <miguel_axt@estudante.sesisenai.org.br>
2025-03-26 08:38:23 -0300

    ADD - laco while

--- script.js
+++ script.js
@@ -99,3 +99,14 @@ function converterTemperatura(temperatura){
 }
 
 converterTemperatura(15)
+
+
+let soma = 0;
+let i = 1;
+
+while (i <= 100) {
+    soma += i;
+    i++;
+}
+
+console.log("O valor total da soma de 1 a 100 é: " + soma);
